3c3f15708afdcb14dd88c713861e596e017e4184
liweijun <lwj@liweijuns-MacBook-Pro.local>
2022-04-20 11:00:05 +0800

    mac

--- insert_sql.php
+++ insert_sql.php
@@ -23,9 +23,39 @@ if($_POST['table'] == "vaccine"){
     $sql =  "insert into vaccine(vaccine_id, vaccine_name, vaccine_manufacture) values($type1, $type2, $type3)";
 //    echo "$sql";
     if ($link->query($sql) === TRUE) {
-        echo "插入成功";
+        echo "（vaccine表）插入成功";
     } else {
-        echo "插入失败<br>";
+        echo "（vaccine表）插入失败<br>";
+    }
+}elseif ($_POST['table'] == "institute"){
+    $type1 = (int)$_POST['institute_id'];
+    $type2 = "'".$_POST['institute_name']."'";
+    $type3 = "'".$_POST['institute_tel']."'";
+    $type4 = (int)$_POST['institute_StudentNumber'];
+
+    $sql = "insert into institute(institute_id, institute_name, institute_tel, institute_StudentNumber) values($type1, $type2, $type3, $type4)";
+
+    if ($link->query($sql) === TRUE) {
+        echo "（institute表）插入成功";
+    } else {
+        echo "（institute表）插入失败<br>";
+    }
+}elseif ($_POST['table'] == "student"){
+    $type1 = (int)$_POST['student_id'];
+    $type2 = "'".$_POST['student_name']."'";
+    $type3 = "'".$_POST['student_sex']."'";
+    $type4 = "'".$_POST['student_tel']."'";
+    $type5 = (int)$_POST['institute_id'];
+    $type6 = (int)$_POST['is_vaccine'];
+    $type7 = (int)$_POST['is_NAT'];
+    $type8 = (int)$_POST['vaccine_id'];
+
+    $sql = "insert into student(student_id, student_name, student_sex, student_tel, institute_id, is_vaccine, is_NAT, vaccine_id) values($type1, $type2, $type3, $type4, $type5, $type6, $type7, $type8)";
+
+    if ($link->query($sql) === TRUE) {
+        echo "（student表）插入成功";
+    } else {
+        echo "（student表）插入失败<br>";
     }
 }
 
--- update.php
+++ update.php
@@ -2,10 +2,23 @@
 // var_dump($_POST);
 $res = explode(" ", $_POST['id']);
 $id = $res[0];
-$table = $res[1];
-// echo $id." and ".$table;
 
-if($table == "t_vaccine"){ ?>
+$tmp = explode("_",$res[1]);
+$table = $tmp[1];
+
+
+include "conn.php";
+$link = $GLOBALS['conn'];
+$link->query("use vaccine");
+
+if($res[1] == "t_vaccine"){
+    $sql_vaccine = "select * from $table where vaccine_id = $id";
+    $data = $link->query($sql_vaccine);
+    $row = $data->fetch_assoc();
+    $vaccine_id = $row['vaccine_id'];
+    $vaccine_name = $row['vaccine_name'];
+    $vaccine_manufacture = $row['vaccine_manufacture'];
+    ?>
     <table border="1">
         <tr>
             <td>疫苗编号</td>
@@ -13,15 +26,23 @@ if($table == "t_vaccine"){ ?>
             <td>生产厂家</td>
         </tr>
         <tr>
-            <form>
-                <td> <input type="number"> </td>
-                <td> <input type="text"> </td>
-                <td> <input type="text"> </td>
-                <td> <input type="submit" name="op" value="update"> </td>
+            <form action="update_sql.php" method="post">
+                <td> <input type="number" name="vaccine_id" value="<?php echo $vaccine_id ?>"> </td>
+                <td> <input type="text" name="vaccine_name" value="<?php echo $vaccine_name ?>"> </td>
+                <td> <input type="text" name="vaccine_manufacture" value="<?php echo $vaccine_manufacture ?>"> </td>
+                <td> <button type="submit" name="table" value="<?php echo "vaccine"." ".$id ?>">修改</button></td>
             </form>
         </tr>
     </table>
-<?php }elseif($table == "t_institute"){ ?>
+<?php }elseif($res[1] == "t_institute"){
+    $sql_institute = "select * from $table where institute_id = $id";
+    $data = $link->query($sql_institute);
+    $row = $data->fetch_assoc();
+    $institute_id = $row['institute_id'];
+    $institute_name = $row['institute_name'];
+    $institute_tel = $row['institute_tel'];
+    $institute_StudentNumber = $row['institute_StudentNumber'];
+    ?>
     <table border="1">
         <tr>
             <td>学院号</td>
@@ -30,16 +51,16 @@ if($table == "t_vaccine"){ ?>
             <td>学院人数</td>
         </tr>
         <tr>
-            <form>
-                <td> <input type="number"> </td>
-                <td> <input type="text"> </td>
-                <td> <input type="text"> </td>
-                <td> <input type="number"> </td>
-                <td> <input type="submit" name="op" value="update"> </td>
+            <form action="update_sql.php" method="post">
+                <td> <input type="number" name="institute_id" value="<?php echo $institute_id ?>"> </td>
+                <td> <input type="text" name="institute_name"> </td>
+                <td> <input type="text" name="institute_tel"> </td>
+                <td> <input type="number" name="institute_StudentNumber"> </td>
+                <td> <button type="submit" name="table" value="<?php echo "institute"." ".$id ?>">修改</button> </td>
             </form>
         </tr>
     </table>
-<?php }elseif ($table == "t_student"){ ?>
+<?php }elseif ($res[1] == "t_student"){ ?>
     <table border="1">
         <tr>
             <td>学号</td>
@@ -52,16 +73,16 @@ if($table == "t_vaccine"){ ?>
             <td>接种疫苗的编号</td>
         </tr>
         <tr>
-            <form>
-                <td> <input type="number"> </td>
-                <td> <input type="text"> </td>
-                <td> <input type="text"> </td>
-                <td> <input type="text"> </td>
-                <td> <input type="number"> </td>
-                <td> <input type="number"> </td>
-                <td> <input type="number"> </td>
-                <td> <input type="number"> </td>
-                <td> <input type="submit" name="op" value="update"> </td>
+            <form action="update_sql.php" method="post">
+                <td> <input type="number" name="student_id"> </td>
+                <td> <input type="text" name="student_name"> </td>
+                <td> <input type="text" name="student_sex"> </td>
+                <td> <input type="text" name="student_tel"> </td>
+                <td> <input type="number" name="institute_id"> </td>
+                <td> <input type="number" name="is_vaccine"> </td>
+                <td> <input type="number" name="is_NAT"> </td>
+                <td> <input type="number" name="vaccine_id"> </td>
+                <td> <button type="submit" name="table" value="<?php echo "student"." ".$id ?>">修改</button> </td>
             </form>
         </tr>
     </table>
